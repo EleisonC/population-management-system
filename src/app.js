b259190a77e4ad165ecc3e5d3d56a424cec1a20b
christopherkalule <ckalule7@gmail.com>
2019-05-27 23:19:24 +0300

    express set up

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const {PORT,MLAB_URI} = process.env
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+
+const app = express();
+const db = mongoose.connect(MLAB_URI);
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+
+
+app.listen(4000, () => {
+  console.log('Application running on port 4000...')
+});
+
+module.exports = app
