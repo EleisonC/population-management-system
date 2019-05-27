a0d56542be53a9f82326eba3e7052f42a95f0060
christopherkalule <ckalule7@gmail.com>
2019-05-28 00:03:35 +0300

    set up location schema

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan')
 
 const app = express();
-const db = mongoose.connect(MLAB_URI);
+const db = mongoose.connect(MLAB_URI, { useNewUrlParser: true });
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -13,6 +13,6 @@ app.use(bodyParser.json())
 
 app.listen(4000, () => {
   console.log('Application running on port 4000...')
-});
+})
 
 module.exports = app
